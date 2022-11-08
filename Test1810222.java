6140eb3b77f882d06b3c17990e586e4fc284338d
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-08 12:26:18 +0530

    Add files via upload

---
+++ Test1810222.java
@@ -0,0 +1,10 @@
+// Declare the variable example
+class Test1810222 {
+public static void main(String[] args) {
+	 int a=15;
+	double d=25.71;
+	String s="Hello Anudip"; // variable declaration
+	System.out.println(a+d);
+	System.out.print(s);
+}//main end
+}//clas end
